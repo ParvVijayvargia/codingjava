a2e9286b15f564337d67023e6b6a6f71946eaf0d
ParvVijayvargia <parvvijay150303@gmail.com>
2022-09-25 17:27:00 +0530

    jsk - my third code

--- bunny/Main.java
+++ bunny/Main.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-// check
+// check 2
         System.out.println("Please enter a number");
         int number = scan.nextInt();
         System.out.println (" sum is " + printTable(number));
