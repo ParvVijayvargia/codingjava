303958a3c062e8eb85a43c821bbd20d989c22d37
ParvVijayvargia <parvvijay150303@gmail.com>
2022-09-25 17:21:46 +0530

    jsk - my first code

---
+++ bunny/Main.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Please enter a number");
+        int number = scan.nextInt();
+        System.out.println (" sum is " + printTable(number));
+
+    }
+    private static int printTable(int number)
+    {
+        int sum=0;
+        for(int i=0;i<10;i++)
+        {
+            sum= sum+number*(i+1);
+            System.out.println(number + " * " + (i+1) + "= " + number*(i+1));
+        }
+
+        return sum;
+    }
+
+}
